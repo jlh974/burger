e80a110baf64865fb5585cb207ea47d3363e7f1d
Jeannine Hall <jeanninehall@Jeannines-MacBook-Pro.local>
2017-05-06 07:39:05 -0500

    intermediate commit

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+	id INTEGER AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(100) NOT NULL,
+	devoured BOOLEAN NOT NULL,
+	day TIMESTAMP NOT NULL,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("hamburger");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon cheeseburger");
